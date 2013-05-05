12247bbfa23cb64f0792b07588013e22b1f9ab2e
Corey Woodcox <corey.woodcox@gmail.com>
2013-05-04 19:26:14 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Watersupply::Application.config.secret_key_base = 'cde7ca983a9a8e04ea43ebbfb55d8bc64a318aa6d3be954cfeff6a66409a2a68b5899121d526f1264523f634899f5907501b0d734f22d5fa041127aa5751d7c5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Watersupply::Application.config.session_store :cookie_store, key: '_watersupply_session'
